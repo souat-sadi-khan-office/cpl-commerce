c692ece297a034bb898eada0d0674c4a9587be94
= <=>
2024-10-27 15:07:13 +0600

    first commit

---
+++ resources/views/frontend/homepage/trending.blade.php
@@ -0,0 +1,139 @@
+<!-- START SECTION SHOP -->
+<div class="section small_pt small_pb">
+	<div class="custom-container">
+    	<div class="row">
+        	<div class="col-xl-3 d-none d-xl-block">
+            	<div class="sale-banner">
+                	<a class="hover_effect1" href="#">
+                		<img src="{{asset('frontend/assets/images/shop_banner_img10.jpg')}}" alt="shop_banner_img10">
+                    </a>
+                </div>
+            </div>
+        	<div id="trending-product-area" class="col-xl-9">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="heading_tab_header">
+                            <div class="heading_s2">
+                                <h4>Trending products</h4>
+                            </div>
+                            <div class="view_all">
+                            	<a href="{{ route('search', ['sort' => 'popularity']) }}" class="text_default"><i class="linearicons-power"></i> <span>View All</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="trending_slider owl-carousel owl-theme dot_style1" data-loop="true" data-margin="20" data-responsive='{"0":{"items": "1"}, "481":{"items": "2"}, "768":{"items": "3"}, "991":{"items": "4"}}'>
+                            <div class="item">
+                                <div class="product_wrap placeholder">
+                                    <div class="product_img">
+                                        <a href="javascript:;">
+                                            <div class="img_placeholder loading"></div>
+                                        </a>
+                                    </div>
+                                    <div class="product_info">
+                                        <h6 class="product_title">
+                                            <div class="title_placeholder loading"></div>
+                                        </h6>
+                                        <div class="product_price">
+                                            <div class="price_placeholder loading"></div>
+                                            <del>
+                                                <div class="del_placeholder loading"></div>
+                                            </del>
+                                        </div>
+                                        <div class="rating_wrap">
+                                            <div class="rating_placeholder loading"></div>
+                                        </div>
+                                        <div class="pr_desc">
+                                            <div class="desc_placeholder loading"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="product_wrap placeholder">
+                                    <div class="product_img">
+                                        <a href="javascript:;">
+                                            <div class="img_placeholder loading"></div>
+                                        </a>
+                                    </div>
+                                    <div class="product_info">
+                                        <h6 class="product_title">
+                                            <div class="title_placeholder loading"></div>
+                                        </h6>
+                                        <div class="product_price">
+                                            <div class="price_placeholder loading"></div>
+                                            <del>
+                                                <div class="del_placeholder loading"></div>
+                                            </del>
+                                        </div>
+                                        <div class="rating_wrap">
+                                            <div class="rating_placeholder loading"></div>
+                                        </div>
+                                        <div class="pr_desc">
+                                            <div class="desc_placeholder loading"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="product_wrap placeholder">
+                                    <div class="product_img">
+                                        <a href="javascript:;">
+                                            <div class="img_placeholder loading"></div>
+                                        </a>
+                                    </div>
+                                    <div class="product_info">
+                                        <h6 class="product_title">
+                                            <div class="title_placeholder loading"></div>
+                                        </h6>
+                                        <div class="product_price">
+                                            <div class="price_placeholder loading"></div>
+                                            <del>
+                                                <div class="del_placeholder loading"></div>
+                                            </del>
+                                        </div>
+                                        <div class="rating_wrap">
+                                            <div class="rating_placeholder loading"></div>
+                                        </div>
+                                        <div class="pr_desc">
+                                            <div class="desc_placeholder loading"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="product_wrap placeholder">
+                                    <div class="product_img">
+                                        <a href="javascript:;">
+                                            <div class="img_placeholder loading"></div>
+                                        </a>
+                                    </div>
+                                    <div class="product_info">
+                                        <h6 class="product_title">
+                                            <div class="title_placeholder loading"></div>
+                                        </h6>
+                                        <div class="product_price">
+                                            <div class="price_placeholder loading"></div>
+                                            <del>
+                                                <div class="del_placeholder loading"></div>
+                                            </del>
+                                        </div>
+                                        <div class="rating_wrap">
+                                            <div class="rating_placeholder loading"></div>
+                                        </div>
+                                        <div class="pr_desc">
+                                            <div class="desc_placeholder loading"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        	</div>
+        </div>
+    </div>
+</div>
+<!-- END SECTION SHOP -->
